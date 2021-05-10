3ed780ffdd2860525cd0a09622074db315c0fea5
Germán Escobar <german.escobarc@gmail.com>
2021-05-10 15:31:57 -0500

    Add more options to Chrome

--- lambda_function.rb
+++ lambda_function.rb
@@ -7,7 +7,7 @@ def lambda_handler(event:, context:)
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+    # options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--window-size=1400,1400')
     options.add_argument('--single-process')
     Capybara::Selenium::Driver.new(nil, browser: :chrome, options: options)
